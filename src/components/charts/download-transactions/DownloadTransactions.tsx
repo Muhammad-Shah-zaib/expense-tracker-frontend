d236fef55d8a27bf82a111973df83ec6190db98a
Pace_Setter <132779528+Muhammad-Shah-zaib@users.noreply.github.com>
2025-02-22 15:17:08 +0500

    added MUI datePicker for downloadTransactions (#26)

--- src/components/charts/download-transactions/DownloadTransactions.tsx
+++ src/components/charts/download-transactions/DownloadTransactions.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import Button from "@mui/material/Button";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Dayjs } from "dayjs";
 
 const DownloadTransactions = () => {
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
+  const [endDate, setEndDate] = useState<Dayjs | null>(null);
+
   return (
     <div
       className={`flex flex-col items-center justify-center gap-4 w-full py-4 bg-primary rounded-lg`}
@@ -9,19 +15,22 @@ const DownloadTransactions = () => {
       <span className={`text-secondary font-bold font-mulish`}>Invoices</span>
 
       <form className={`w-full flex flex-col gap-4 px-2`}>
-        <input
-          type={"date"}
-          className={`bg-primary-900 outline-none rounded-xl p-2`}
+        <DatePicker
+          label="Start Date"
+          value={startDate}
+          onChange={(newValue) => setStartDate(newValue)}
+          slotProps={{ textField: { fullWidth: true, variant: "outlined" } }}
         />
         <span
           className={`w-full text-center font-playpen font-bold text-primary-400`}
         >
           to
         </span>
-
-        <input
-          type={"date"}
-          className={`bg-primary-900 outline-none rounded-xl p-2`}
+        <DatePicker
+          label="End Date"
+          value={endDate}
+          onChange={(newValue) => setEndDate(newValue)}
+          slotProps={{ textField: { fullWidth: true, variant: "outlined" } }}
         />
 
         <motion.div
@@ -41,4 +50,5 @@ const DownloadTransactions = () => {
     </div>
   );
 };
+
 export default DownloadTransactions;
