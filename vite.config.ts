67db76a1ae4e6360b193e051c753daabcb7c7c01
Muhammad-Shah-zaib <MuhammdShahzaib@outlook.com>
2025-04-25 01:27:20 +0500

    updated to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt"],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000, // 5 MB, adjust as needed
+      },
+      manifest: {
+        name: "Vizufi: Expense Tracker",
+        short_name: "Vizufi",
+        description:
+          "Vizufi is a simple and easy to use expense tracker app, where you can visualize your expenses and income, add your transactions, and manage your budget.",
+        theme_color: "#ffffff",
+        display: "standalone", // important for full-screen app behavior
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
