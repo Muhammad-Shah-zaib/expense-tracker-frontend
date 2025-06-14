b91ceac01a10366210ec663a92b91118d9c70a79
Pace_Setter <132779528+Muhammad-Shah-zaib@users.noreply.github.com>
2025-06-14 13:54:54 +0500

    resolved responsivity bugs over login and signup pages (#37)

--- src/shared/pages/SignUp.tsx
+++ src/shared/pages/SignUp.tsx
@@ -19,8 +19,8 @@ const Login = () => {
   return (
     <div className="w-[100vw] h-[100vh] bg-primary flex items-center justify-center">
       <div className="shadow-xl shadow-primary-900">
-        <div className="max-w-[780px] max-h-[500px] flex items-center overflow-hidden rounded-sm">
-          <div className="w-[400px] h-[500px] p-8 bg-zinc-900 flex justify-between flex-col gap-4">
+        <div className="max-w-[780px] md:max-h-[500px] flex md:flex-row flex-col  items-center overflow-hidden rounded-sm">
+          <div className="w-[400px] md:h-[500px] p-8 bg-zinc-900 flex justify-between flex-col gap-4">
             <div className="w-full text-center text-5xl font-playpen">
               SignUp
             </div>
